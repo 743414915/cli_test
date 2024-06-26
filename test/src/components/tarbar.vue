9d5850ab90036a4ac2cb833663fe5e28e32cf5d9
wang <743414915@qq.com>
2024-06-26 23:00:30 +0800

    fea：finish

---
+++ test/src/components/tarbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tarbar">
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+<script setup lang="ts">
+  import { ref } from "vue";
+
+  const msg = ref("tarbar");
+</script>
+<style lang="less" scoped>
+  .tarbar {
+    
+  }
+</style>
